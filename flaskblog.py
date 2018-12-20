bfc46ca182e6fbd0e91c77c643f8609aa61042ad
SimonOkello <simonokello93@gmail.com>
2018-12-20 04:17:57 +0300

    Initial Commit

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -27,15 +27,18 @@ def home():
 def about():
     return render_template("about.html", title = "About")
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account has been created for {form.username.data} !", "success")
+        return redirect(url_for("home"))
     return render_template("register.html", title = "Register", form = form)
 
 @app.route("/login")
 def login():
     form = LoginForm()
-    return render_template("register.html", title = "Register", form = form)
+    return render_template("login.html", title = "Login", form = form)
 
 if __name__ == "__main__":
     app.run(debug = True)
